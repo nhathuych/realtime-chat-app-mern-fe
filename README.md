f37c2271413a8e8ef97b7ae0403a1eff1a718528
Huy Huy <nhathuych@gmail.com>
2025-02-22 16:09:40 +0700

    initial

---
+++ README.md
@@ -0,0 +1,11 @@
+# realtime-chat-app-mern-fe
+
+## Init project via vite
+```
+npm create vite@latest .
+```
+
+## Run this command
+```
+npm run dev
+```
---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <div className="font-bold text-yellow-600 text-3xl text-center">Chat app mern stack</div>
+  )
+}
+
+export default App
