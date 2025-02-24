873296773dd06cb403ff10f3472c9a93c70008ea
Huy Huy <nhathuych@gmail.com>
2025-02-24 19:30:03 +0700

    install daisyUI & create blank pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,24 @@
+import { Routes, Route } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import HomePage from "./pages/HomePage"
+import SignupPage from "./pages/SignupPage"
+import LoginPage from "./pages/LoginPage"
+import SettingsPage from "./pages/SettingsPage"
+import ProfilePage from "./pages/ProfilePage"
+
 function App() {
   return (
-    <div className="font-bold text-yellow-600 text-3xl text-center">Chat app mern stack</div>
+    <div>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </div>
   )
 }
 
