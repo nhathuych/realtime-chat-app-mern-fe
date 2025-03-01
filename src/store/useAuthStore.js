0ac95a0afe345c2ff7aa6636a8c2b61b878738c5
Huy Huy <nhathuych@gmail.com>
2025-03-01 11:01:27 +0700

    add socket connection and disconnection

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -1,20 +1,24 @@
 import { create } from "zustand" // Read more: https://www.npmjs.com/package/zustand
 import axiosInstance from "../lib/axiosInstance"
 import toast from "react-hot-toast"
+import { io } from "socket.io-client"
 
 // Because an arrow function () => ({ ... }) is used, the object must be wrapped in () to avoid confusion with a block statement {}
-export const useAuthStore = create((set) => ({
+export const useAuthStore = create((set, get) => ({
   authUser: null,
   isSigningUp: false,
   isLogginIn: false,
   isUpdatingProfile: false,
   isCheckingAuth: true,
   onlineUsers: [],
+  socket: null,
 
   checkAuth: async () => {
     try {
       const res = await axiosInstance.get("/users/current")
       set({ authUser: res.data.user })
+
+      get().connectSocket()
     } catch (error) {
       set({ authUser: null })
       console.log("Error in checkAuth:", error)
@@ -42,6 +46,8 @@ export const useAuthStore = create((set) => ({
       const res = await axiosInstance.post('/auth/login', data)
       set({ authUser: res.data })
       toast.success('You have been logged in successfully')
+
+      get().connectSocket()
     } catch (error) {
       toast.error(error.response.data.message)
     } finally {
@@ -56,6 +62,8 @@ export const useAuthStore = create((set) => ({
       toast.success('You have been logged out successfully')
     } catch (error) {
       toast.error(error.response.data.message)
+    } finally {
+      get().disconnectSocket()
     }
   },
 
@@ -70,5 +78,19 @@ export const useAuthStore = create((set) => ({
     } finally {
       set({ isUpdatingProfile: false })
     }
-  }
+  },
+
+  connectSocket: async () => {
+    const { authUser } = get()
+    if (!authUser || get().socket?.connected) return
+
+    const socket = io(import.meta.env.VITE_SOCKET_HTTP_BASE_URL)
+
+    socket.connect()
+    set({ socket })
+  },
+
+  disconnectSocket: async () => {
+    if (get().socket?.connected) get().socket.disconnect()
+  },
 }))
