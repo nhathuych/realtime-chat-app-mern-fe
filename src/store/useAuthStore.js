363781804d635e802ca642275e68eee621a138e4
Huy Huy <nhathuych@gmail.com>
2025-03-01 13:20:16 +0700

    listen for the "getOnlineUsers" event (from backend) and update onlineUsers state

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -84,10 +84,18 @@ export const useAuthStore = create((set, get) => ({
     const { authUser } = get()
     if (!authUser || get().socket?.connected) return
 
-    const socket = io(import.meta.env.VITE_SOCKET_HTTP_BASE_URL)
+    const socket = io(import.meta.env.VITE_SOCKET_HTTP_BASE_URL, {
+      query: {
+        userId: authUser._id,
+      }
+    })
 
     socket.connect()
     set({ socket })
+
+    socket.on("getOnlineUsers", (userIds) => {
+      set({ onlineUsers: userIds })
+    })
   },
 
   disconnectSocket: async () => {
